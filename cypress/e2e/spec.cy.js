6006353de9f40e92984c6f6ea4d578fddb4cd3bd
Mariaulfa <hallomaria@gmail.com>
2022-11-29 16:03:09 +0700

    Quiz2

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('http://ujian.jti.polinema.ac.id')
+    cy.get('input[name="xuser_name"]').type('2041720073')
+    cy.get('input[name="xuser_password"]').type('2041720073').type("{enter}")
+  })
+})
